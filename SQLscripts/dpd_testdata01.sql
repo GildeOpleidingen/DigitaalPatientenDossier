f2696704629f4eed96ea953256f53a5588e8ca78
Fred Jacobs <f.jacobs@rocgilde.nl>
2023-12-06 09:29:26 +0100

    #91 remove start_transaction in sql test data script

--- SQLscripts/dpd_testdata01.sql
+++ SQLscripts/dpd_testdata01.sql
@@ -1,21 +1,4 @@
--- phpMyAdmin SQL Dump
--- version 5.1.1
--- https://www.phpmyadmin.net/
---
--- Host: 127.0.0.1
--- Gegenereerd op: 06 dec 2023 om 09:21
--- Serverversie: 10.4.22-MariaDB
--- PHP-versie: 8.0.13
 
-SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-START TRANSACTION;
-SET time_zone = "+00:00";
-
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
 
 --
 -- Database: `dpd`
