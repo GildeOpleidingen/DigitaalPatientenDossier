274d7f0b75df65721cf5f79dcdc1786f2e2fa168
Milan <milanensink01@gmail.com>
2023-12-11 13:05:30 +0100

    Changes

--- Functions/MedewerkerFunctions.php
+++ Functions/MedewerkerFunctions.php
@@ -1,12 +1,15 @@
 <?php
 
-//include_once '../Database/DatabaseConnection.php';
 include './Database/DatabaseConnection.php';
 
-function createNewMedewerker($naam, $klas, $foto, $email, $telefoonnummer, $wachtwoord): void {
+//createNewMedewerker("Test", "11", imgData, "test@gmail.com", "310600000000", "test");
+function createNewMedewerker($naam, $klas, $foto, $email, $telefoonnummer, $wachtwoord): bool {
     $result = DatabaseConnection::getConn()->query("SELECT * FROM `medewerker` WHERE naam='${naam}'")->fetch_all();
 
     if (sizeof($result) == 0) {
         DatabaseConnection::getConn()->query("INSERT INTO `medewerker`(`naam`, `klas`, `foto`, `email`, `telefoonnummer`, `wachtwoord`) VALUES ('${naam}','${klas}','${foto}','${email}','${telefoonnummer}','${wachtwoord}')");
+        return true;
     }
+
+    return false;
 }
