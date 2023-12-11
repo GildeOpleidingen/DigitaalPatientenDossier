a9ec34863a76c1395442b321a2bc51a36c9b3ef9
Luca <luca.onano@student.gildeopleidingen.nl>
2023-12-11 11:33:21 +0100

    function gemaakt voor string van 01010001

---
+++ Functions/ConvertAnamnese.php
@@ -0,0 +1,31 @@
+<?php
+
+function convertNumToBool($numbers, $index=0): ?bool {
+    if ($index <= mb_strlen($numbers)) {
+        $num = str_split($numbers)[$index];
+
+        if ($num == 1) {
+            return true;
+        }
+
+        return false;
+    }
+
+    return null;
+}
+
+function convertNumToBoolArray($numbers): array {
+    $numArr = str_split($numbers);
+    $boolArr = array();
+
+    foreach ($numArr as $num) {
+        if ($num == 1) {
+            $boolArr[] = true;
+            continue;
+        }
+
+        $boolArr[] = false;
+    }
+
+    return $boolArr;
+}
