30e52ce1455c5c5c5cff0772492fbc92898af4f3
Tijn-Janssen05 <Tijn.Janssen@student.GildeOpleidingen.nl>
2023-12-11 13:38:02 +0100

    Zoek functie op naam gemaakt!

--- Cliënt/clientpagina/clientoverzicht.php
+++ Cliënt/clientpagina/clientoverzicht.php
@@ -1,9 +1,6 @@
 <?php 
 include '../../Database/DatabaseConnection.php';
 $result = DatabaseConnection::getConn()->query("SELECT id, naam, woonplaats, geboortedatum FROM client;");
-  
-    
-
 ?>
 
 <!DOCTYPE html>
@@ -14,6 +11,10 @@ $result = DatabaseConnection::getConn()->query("SELECT id, naam, woonplaats, geb
     <title>Cliënten</title>
 </head>
 <body>
+    <form action="patientenzoeken.php" method="post">
+        Search <input type="text" name ="search">
+        <input type ="submit">
+    </form>
     <center>
         <table>
             <tr>
@@ -24,13 +25,25 @@ $result = DatabaseConnection::getConn()->query("SELECT id, naam, woonplaats, geb
             </tr>
             <?php while($row1 = mysqli_fetch_array($result)):;?>
             <tr>
-                <td><?php echo $row1[0];?></td>
-                <td><?php echo $row1[1];?></td>
-                <td><?php echo $row1[2];?></td>
-                <td><?php echo $row1[3];?></td>
+                <td class="row1"><?php echo $row1[0];?></td>
+                <td class="row1"><?php echo $row1[1];?></td>
+                <td class="row1"><?php echo $row1[2];?></td>
+                <td class="row1"><?php echo $row1[3];?></td>
             </tr>
             <?php endwhile;?>
         </table>
-    </center>
 </body>
 </html>
+
+<style> 
+th {
+  border: 1px solid black;
+  border-radius: 10px;
+  width: 150px;
+}
+
+.row1{
+  border: 1px solid black;
+  border-radius: 10px;
+}
+</style>
