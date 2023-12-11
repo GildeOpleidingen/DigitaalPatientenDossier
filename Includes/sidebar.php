bf518cd29f6916770e6069ffe6f28b986ada6919
Flamex <105858527+Flamexyy@users.noreply.github.com>
2023-12-11 14:24:36 +0100

    homepage template

---
+++ Includes/sidebar.php
@@ -0,0 +1,35 @@
+<html>
+	<head>
+		<meta charset="utf-8">
+		<link rel="Stylesheet" href="includes/sidebar.css">
+	</head>
+  
+	<body>
+        <div class='container'>
+            <div class="sidebar">
+                <div class="profile">
+                    <div class="profile-img">
+                        <img src="Images\blank-avatar-photo-place-holder-600nw-1095249842.webp" alt="profile picture">
+                    </div>
+                    <div class="personal-information">
+                        <p>Voornaam Achternaam</p>
+                        <p id="birth">00-00-0000 (00 Jaar)</p>
+                    </div>
+                </div>
+                    <div class="cpr">
+                        <p>Wel reanimeren</p>
+                    </div>
+                
+                <ul>
+                    <a href="#">Overzicht</a>
+                    <a href="#">Patiëntgegevens</a>
+                    <a href="#">Anamnese</a>
+                    <a href="#">Zorgplan</a>
+                    <a href="#">Rapportage</a>
+                    <a href="#">Metingen</a>
+                    <a href="#">Formulieren</a>
+                </ul>
+            </div>
+        </div>
+	</body>
+</html>
