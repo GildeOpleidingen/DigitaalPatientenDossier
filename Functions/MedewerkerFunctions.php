ee98f542a045bde591345c36f6f53e55529b108c
Milan <milanensink01@gmail.com>
2023-12-14 10:57:32 +0100

    Changes to medewerker function

--- Functions/MedewerkerFunctions.php
+++ Functions/MedewerkerFunctions.php
@@ -1,18 +1,22 @@
 <?php
 
-include './Database/DatabaseConnection.php';
+include_once './Database/DatabaseConnection.php';
 
-//createNewMedewerker("Test", "11", imgData, "test@gmail.com", "310600000000", "test");
 function updateMedewerker($naam, $klas, $foto, $email, $telefoonnummer, $wachtwoord): bool {
     $conn = DatabaseConnection::getConn();
     $conn->query("UPDATE `medewerker` SET `naam`='${naam}',`klas`='${klas}',`foto`='${foto}',`email`='${email}',`telefoonnummer`='${telefoonnummer}',`wachtwoord`='${wachtwoord}' WHERE `naam`='${naam}';");
 
+    echo $conn->affected_rows;
     if ($conn->affected_rows == 1)
         return true;
 
-    if ($conn->affected_rows < 0) {
-        $conn->query("INSERT INTO `medewerker`(`naam`, `klas`, `foto`, `email`, `telefoonnummer`, `wachtwoord`) VALUES ('${naam}','${klas}','${foto}','${email}','${telefoonnummer}','${wachtwoord}')");
-        return true;
+    if ($conn->affected_rows <= 0) {
+        $result = $conn->query("SELECT * FROM `medewerker` WHERE naam='${naam}'")->fetch_all();
+        if (sizeof($result) == 0) {
+            $conn->query("INSERT INTO `medewerker`(`naam`, `klas`, `foto`, `email`, `telefoonnummer`, `wachtwoord`) VALUES ('${naam}','${klas}','${foto}','${email}','${telefoonnummer}','${wachtwoord}')");
+            return true;
+
+        }
     }
 
     return false;
