6db8ae5463ced874975fa5a025b5ea4632675c8c
Milan Ensink <65659381+Yiixez@users.noreply.github.com>
2023-12-15 09:26:37 +0100

    #135 update client functie (#139)
    * Begin functie
    
    * Changes
    
    * Changes + Fixes
    
    * methode naam change
    
    * added client update/create function
    
    * Changes to medewerker function
    
    * removed unnecessary echo's

---
+++ Functions/ClientFunctions.php
@@ -0,0 +1,21 @@
+<?php
+
+include_once './Database/DatabaseConnection.php';
+
+function updateClient($naam, $geslacht, $adres, $postcode, $woonplaats, $telefoonnummer, $email, $reanimatiestatus, $nationaliteit, $afdeling, $burgelijkestaat, $foto): bool {
+    $conn = DatabaseConnection::getConn();
+    $conn->query("UPDATE `client` SET `naam`='${naam}',`geslacht`='${geslacht}',`adres`='${adres}',`postcode`='${postcode}',`woonplaats`='${woonplaats}',`telefoonnummer`='${telefoonnummer}',`email`='${email}',`reanimatiestatus`='${reanimatiestatus}',`nationaliteit`='${nationaliteit}',`afdeling`='${afdeling}',`burgelijkestaat`='${burgelijkestaat}',`foto`='${foto}' WHERE `naam`='${naam}';");
+
+    if ($conn->affected_rows == 1)
+        return true;
+
+    if ($conn->affected_rows <= 0) {
+        $result = $conn->query("SELECT * FROM `client` WHERE naam='${naam}'")->fetch_all();
+        if (sizeof($result) == 0) {
+            $conn->query("INSERT INTO `client`(`naam`, `geslacht`, `adres`, `postcode`, `woonplaats`, `telefoonnummer`, `email`, `reanimatiestatus`, `nationaliteit`, `afdeling`, `burgelijkestaat`, `foto`) VALUES ('${naam}','${geslacht}','${adres}','${postcode}','${woonplaats}','${telefoonnummer}','${email}','${reanimatiestatus}','${nationaliteit}','${afdeling}','${burgelijkestaat}','${foto}');");
+            return true;
+        }
+    }
+
+    return false;
+}
