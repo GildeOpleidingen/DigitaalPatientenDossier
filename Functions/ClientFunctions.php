3163a149bbed31a0c4b557bf9bb04d7e94ae5b74
Mike <96177454+Vertilix@users.noreply.github.com>
2023-12-15 10:07:16 +0100

    #143 select client by id name (#144)
    * Begin functie
    
    * Changes
    
    * Changes + Fixes
    
    * methode naam change
    
    * added client update/create function
    
    * Changes to medewerker function
    
    * Add getClientById en getClientByName functies
    
    * Remove deprecated brackets en add check of client bestaat
    
    ---------
    
    Co-authored-by: Milan <milanensink01@gmail.com>
    Co-authored-by: Milan Ensink <65659381+Yiixez@users.noreply.github.com>

--- Functions/ClientFunctions.php
+++ Functions/ClientFunctions.php
@@ -4,18 +4,46 @@ include_once './Database/DatabaseConnection.php';
 
 function updateClient($naam, $geslacht, $adres, $postcode, $woonplaats, $telefoonnummer, $email, $reanimatiestatus, $nationaliteit, $afdeling, $burgelijkestaat, $foto): bool {
     $conn = DatabaseConnection::getConn();
-    $conn->query("UPDATE `client` SET `naam`='${naam}',`geslacht`='${geslacht}',`adres`='${adres}',`postcode`='${postcode}',`woonplaats`='${woonplaats}',`telefoonnummer`='${telefoonnummer}',`email`='${email}',`reanimatiestatus`='${reanimatiestatus}',`nationaliteit`='${nationaliteit}',`afdeling`='${afdeling}',`burgelijkestaat`='${burgelijkestaat}',`foto`='${foto}' WHERE `naam`='${naam}';");
+    $conn->query("UPDATE `client` SET `naam`='$naam',`geslacht`='$geslacht',`adres`='$adres',`postcode`='$postcode',`woonplaats`='$woonplaats',`telefoonnummer`='$telefoonnummer',`email`='$email',`reanimatiestatus`='$reanimatiestatus',`nationaliteit`='$nationaliteit',`afdeling`='$afdeling',`burgelijkestaat`='$burgelijkestaat',`foto`='$foto' WHERE `naam`='$naam';");
 
     if ($conn->affected_rows == 1)
         return true;
 
     if ($conn->affected_rows <= 0) {
-        $result = $conn->query("SELECT * FROM `client` WHERE naam='${naam}'")->fetch_all();
+        $result = $conn->query("SELECT * FROM `client` WHERE naam='$naam'")->fetch_all();
         if (sizeof($result) == 0) {
-            $conn->query("INSERT INTO `client`(`naam`, `geslacht`, `adres`, `postcode`, `woonplaats`, `telefoonnummer`, `email`, `reanimatiestatus`, `nationaliteit`, `afdeling`, `burgelijkestaat`, `foto`) VALUES ('${naam}','${geslacht}','${adres}','${postcode}','${woonplaats}','${telefoonnummer}','${email}','${reanimatiestatus}','${nationaliteit}','${afdeling}','${burgelijkestaat}','${foto}');");
+            $conn->query("INSERT INTO `client`(`naam`, `geslacht`, `adres`, `postcode`, `woonplaats`, `telefoonnummer`, `email`, `reanimatiestatus`, `nationaliteit`, `afdeling`, `burgelijkestaat`, `foto`) VALUES ('$naam','$geslacht','$adres','$postcode','$woonplaats','$telefoonnummer','$email','$reanimatiestatus','$nationaliteit','$afdeling','$burgelijkestaat','$foto');");
             return true;
         }
     }
 
     return false;
+}
+
+function checkIfClientExistsById(int $id): bool {
+    $result = getClientById($id);
+
+    return sizeof((array) $result) > 0;
+}
+
+function checkIfClientExistsByName(string $naam): bool {
+    $result = getClientByName($naam);
+
+    return sizeof((array) $result) > 0;
+}
+
+function getClientById($id): array|null {
+    $result = DatabaseConnection::getConn()->prepare("SELECT * FROM `client` WHERE id = ?;");
+    $result->bind_param("i", $id);
+    $result->execute();
+
+    return $result->get_result()->fetch_array();
+}
+
+function getClientByName($name): array|null {
+    $result = DatabaseConnection::getConn()->prepare("SELECT * FROM `client` WHERE naam = ?;");
+    $result->bind_param("s", $name);
+    $result->execute();
+
+    return $result->get_result()->fetch_array();
 }
