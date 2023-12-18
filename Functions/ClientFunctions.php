222b558cb1366571c42465f2b9608341c79f247a
Mike <mike.van.pol@student.gildeopleidingen.nl>
2023-12-18 12:56:38 +0100

    Add insertClientStory function en getMedischOverzichtByClientId function #25

--- Functions/ClientFunctions.php
+++ Functions/ClientFunctions.php
@@ -1,7 +1,4 @@
 <?php
-
-include_once './Database/DatabaseConnection.php';
-
 function updateClient($naam, $geslacht, $adres, $postcode, $woonplaats, $telefoonnummer, $email, $reanimatiestatus, $nationaliteit, $afdeling, $burgelijkestaat, $foto): bool {
     $conn = DatabaseConnection::getConn();
     $conn->query("UPDATE `client` SET `naam`='$naam',`geslacht`='$geslacht',`adres`='$adres',`postcode`='$postcode',`woonplaats`='$woonplaats',`telefoonnummer`='$telefoonnummer',`email`='$email',`reanimatiestatus`='$reanimatiestatus',`nationaliteit`='$nationaliteit',`afdeling`='$afdeling',`burgelijkestaat`='$burgelijkestaat',`foto`='$foto' WHERE `naam`='$naam';");
@@ -11,7 +8,7 @@ function updateClient($naam, $geslacht, $adres, $postcode, $woonplaats, $telefoo
 
     if ($conn->affected_rows <= 0) {
         $result = $conn->query("SELECT * FROM `client` WHERE naam='$naam'")->fetch_all();
-        if (sizeof($result) == 0) {
+        if (sizeof($result) == 0)    {
             $conn->query("INSERT INTO `client`(`naam`, `geslacht`, `adres`, `postcode`, `woonplaats`, `telefoonnummer`, `email`, `reanimatiestatus`, `nationaliteit`, `afdeling`, `burgelijkestaat`, `foto`) VALUES ('$naam','$geslacht','$adres','$postcode','$woonplaats','$telefoonnummer','$email','$reanimatiestatus','$nationaliteit','$afdeling','$burgelijkestaat','$foto');");
             return true;
         }
@@ -26,24 +23,38 @@ function checkIfClientExistsById(int $id): bool {
     return sizeof((array) $result) > 0;
 }
 
-function checkIfClientExistsByName(string $naam): bool {
-    $result = getClientByName($naam);
+function checkIfClientExistsByName(string $name): bool {
+    $result = getClientByName($name);
 
     return sizeof((array) $result) > 0;
 }
 
-function getClientById($id): array|null {
+function getClientById($id): array {
     $result = DatabaseConnection::getConn()->prepare("SELECT * FROM `client` WHERE id = ?;");
     $result->bind_param("i", $id);
     $result->execute();
 
-    return $result->get_result()->fetch_array();
+    return (array) $result->get_result()->fetch_array();
 }
 
-function getClientByName($name): array|null {
+function getClientByName($name): array {
     $result = DatabaseConnection::getConn()->prepare("SELECT * FROM `client` WHERE naam = ?;");
     $result->bind_param("s", $name);
     $result->execute();
 
-    return $result->get_result()->fetch_array();
+    return (array) $result->get_result()->fetch_array();
+}
+
+function getMedischOverzichtByClientId($id): array {
+    $result = DatabaseConnection::getConn()->prepare("SELECT * FROM `medischoverzicht` WHERE clientid = ?;");
+    $result->bind_param("i", $id);
+    $result->execute();
+
+    return (array) $result->get_result()->fetch_array();
+}
+
+function insertClientStory($medischoverzichtid, $foto, $introductie, $familie, $hobbys, $omgang): void {
+    $result = DatabaseConnection::getConn()->prepare("INSERT INTO `clientverhaal`(`id`, `medischoverzichtid`, `foto`, `introductie`, `gezinfamilie`, `belangrijkeinfo`, `hobbies`) VALUES (NULL, ?, ?, ?, ?, ?, ?);");
+    $result->bind_param("ibssss", $medischoverzichtid, $foto, $introductie, $familie, $hobbys, $omgang);
+    $result->execute();
 }
