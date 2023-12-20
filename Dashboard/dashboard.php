6470f4b28f94da00e6c17fa6e87c6892c524a8a3
Milan <milanensink01@gmail.com>
2023-12-20 11:05:39 +0100

    smoll fix

--- Dashboard/dashboard.php
+++ Dashboard/dashboard.php
@@ -1,7 +1,8 @@
 <?php
     include_once '../Database/DatabaseConnection.php';
+    $id = $_GET['id'];
+    $_SESSION['id'] = $_GET['id'];
 
-    $id = $_SESSION['id'] = $_GET['id'];
     $result = DatabaseConnection::getConn()->prepare("SELECT clientid FROM verzorgerregel WHERE medewerkerid = ?");
     $result->bind_param("s", $id);
     $result->execute();
