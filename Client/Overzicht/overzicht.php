e6671cd24cad29edf105422d0a0e8aecac4b2917
Rens <Rens.van.Rijt@Student.GildeOpleidingen.nl>
2024-01-25 11:54:02 +0100

    Verzorgers in overzicht (#177)

--- Client/Overzicht/overzicht.php
+++ Client/Overzicht/overzicht.php
@@ -9,45 +9,101 @@ if(!isset($_GET['id'])) {
 $id = $_GET['id'];
 $_SESSION['clientId'] = $_GET['id'];
 
-$result = DatabaseConnection::getConn()->prepare("SELECT * FROM client WHERE id = ?");
-$result->bind_param("s", $id);
-$result->execute();
-$client = $result->get_result()->fetch_assoc();
+$client = DatabaseConnection::getConn()->prepare("SELECT * FROM client WHERE id = ?");
+$client->bind_param("i", $id);
+$client->execute();
+$client = $client->get_result()->fetch_assoc();
+
+$clientRelations = DatabaseConnection::getConn()->prepare("SELECT * FROM verzorgerregel WHERE clientid = ?");
+$clientRelations->bind_param("i", $id);
+$clientRelations->execute();
+$clientRelations = $clientRelations->get_result()->fetch_all(MYSQLI_ASSOC);
+
+$verzorgers = [];
+foreach ($clientRelations as $relation) {
+    $verzorger = DatabaseConnection::getConn()->prepare("SELECT * FROM medewerker WHERE id = ?");
+    $verzorger->bind_param("i", $relation['medewerkerid']);
+    $verzorger->execute();
+    $verzorger = $verzorger->get_result()->fetch_assoc();
+    array_push($verzorgers, $verzorger);
+}
 ?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="overzicht.css">
-    <title>Overzicht van <?= $client['naam']; ?></title>
+    <link rel="Stylesheet" href="overzicht.css">
+    <title>Overzicht van <?= $client['naam'] ?></title>
 </head>
 <body>
-<?php
-include_once '../../Includes/header.php';
-
-?>
 <div class="main">
-    <?php
-    include_once '../../Includes/sidebar.php';
-    ?>
-    <div class="main2">
-        <div class="client">
-            <?php
-            echo "<div class='data'><pre class='datakey'>Geslacht</pre><div class='datavalue'>${client['geslacht']}</div></div>";
-            echo "<div class='data'><pre class='datakey'>Geboortedatum</pre><div class='datavalue'>${client['geboortedatum']}</div></div>";
-            echo "<div class='data'><pre class='datakey'>Adres</pre><div class='datavalue'>${client['adres']}</div></div>";
-            echo "<div class='data'><pre class='datakey'>Postcode</pre><div class='datavalue'>${client['postcode']}</div></div>";
-            echo "<div class='data'><pre class='datakey'>Woonplaats</pre><div class='datavalue'>${client['woonplaats']}</div></div>";
-            echo "<div class='data'><pre class='datakey'>Telefoonnummer</pre><div class='datavalue'>${client['telefoonnummer']}</div></div>";
-            echo "<div class='data'><pre class='datakey'>Email</pre><div class='datavalue'>${client['email']}</div></div>";
-            echo "<div class='data'><pre class='datakey'>Afdeling</pre><div class='datavalue'>${client['afdeling']}</div></div>";
-            echo "<div class='data'><pre class='datakey'>Nationaliteit</pre><div class='datavalue'>${client['nationaliteit']}</div></div>";
-            echo "<div class='data'><pre class='datakey'>Burgerlijke staat</pre><div class='datavalue'>${client['burgelijkestaat']}</div></div>";
-            ?>
-        </div>
+        <?php
+        include '../../Includes/header.php';
+        ?>
+
+        <?php
+        include '../../Includes/sidebar.php';
+        ?>
+
+        <div class="content">
+                <div class="overzicht">
+                    <div class="overzicht-content">
+                        <div class="text">
+                            <strong>Geslacht</strong>
+                            <p><?= $client['geslacht'] ?></p>
+                        </div>
+                        <div class="text">
+                            <strong>Geboortedatum</strong>
+                            <p><?= $client['geboortedatum'] ?></p>
+                        </div>
+                        <div class="text">
+                            <strong>Adres</strong>
+                            <p><?= $client['adres'] ?></p>
+                        </div>
+                        <div class="text">
+                            <strong>Postcode</strong>
+                            <p><?= $client['postcode'] ?></p>
+                        </div>
+                        <div class="text">
+                            <strong>Woonplaats</strong>
+                            <p><?= $client['woonplaats'] ?></p>
+                        </div>
+                    </div>
+                    <br>
+                    <div class="overzicht-content">
+                        <div class="text">
+                            <strong>Telefoonnummer</strong>
+                            <p><?= $client['telefoonnummer'] ?></p>
+                        </div>
+                        <div class="text">
+                            <strong>E-mail</strong>
+                            <p><?= $client['email'] ?></p>
+                        </div>
+                        <div class="text">
+                            <strong>Afdeling</strong>
+                            <p><?= $client['afdeling'] ?></p>
+                        </div>
+                        <div class="text">
+                            <strong>Burgelijke staat</strong>
+                            <p><?= $client['burgelijkestaat'] ?></p>
+                        </div>
+                        <div class="text">
+                            <strong>Nationaliteit</strong>
+                            <p><?= $client['nationaliteit'] ?></p>
+                        </div>
+                        <div class="text">
+                            <a href="verzorgers.php?id=<?= $_GET['id']?>">
+                                <strong>Verzorger(s)</strong>
+                            </a>
+                            <?php foreach ($verzorgers as $verzorger) { ?>
+                                <p><?= $verzorger['naam'] ?></p>
+                            <?php } ?>
+                        </div>
+                    </div>
+                </div>
+            </div>
     </div>
-</div>
-</div>
+
 </body>
 </html>
