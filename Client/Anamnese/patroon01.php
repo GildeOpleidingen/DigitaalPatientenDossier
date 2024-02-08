01f3a964c5c0e04b772dafed194c42df165d0760
Flamex <105858527+Flamexyy@users.noreply.github.com>
2024-02-08 11:08:45 +0100

    Patronen front-end

--- Client/Anamnese/patroon01.php
+++ Client/Anamnese/patroon01.php
@@ -1,3 +1,4 @@
+
 <?php
 session_start();
 include '../../Database/DatabaseConnection.php';
@@ -10,7 +11,7 @@ include '../../Database/DatabaseConnection.php';
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<link rel="Stylesheet" href="patronen.css">
-    <title>Formulieren</title>
+    <title>Anamnese</title>
 </head>
 <body>
     <div class="main">
@@ -21,6 +22,8 @@ include '../../Database/DatabaseConnection.php';
 
         <?php
             include '../../Includes/sidebar.php';
+
+            
         ?>
 
         <div class="content">
@@ -28,40 +31,69 @@ include '../../Database/DatabaseConnection.php';
             <div class="pages">1 Patroon van gezondheidsbeleving en -instandhouding</div>
                 <div class="form">
                     <div class="questionnaire">
-                        <div class="question"><p>Hoe is uw gezondheid in het algemeen?</p><input type="text"></div>
-                        <div class="question"><p>Wat doet u om gezond te blijven?</p><input type="text"></div>
+                        <div class="question"><p>Hoe is uw gezondheid in het algemeen?</p><textarea  rows="1" cols="25" type="text"></textarea></div>
+                        <div class="question"><p>Wat doet u om gezond te blijven?</p><textarea  rows="1" cols="25" type="text"></textarea></div>
                         <div class="question"><p>- Rookt u?</p>
                             <div class="checkboxes">
-                                <div><input type="checkbox"> Ja</div>
-                                <div><input type="checkbox"> Nee</div>
+                                <div class="question-answer">
+                                    <input id="radio" type="radio" name="radio-1">
+                                    <label>Ja</label>
+                                    <textarea  rows="1" cols="25" id="checkfield" type="text"></textarea>
+                                </div>
+                                <p>
+                                    <input type="radio" name="radio-1">
+                                    <label>Nee</label>
+                                </p>
                             </div>
                         </div>
                         <div class="question"><p>- Drinkt u?</p>
-                            <div class="checkboxes">
-                                <div><input type="checkbox"> Ja</div>
-                                <div><input type="checkbox"> Nee</div>
+                             <div class="checkboxes">
+                                <div class="question-answer">
+                                    <input id="radio" type="radio" name="radio-2">
+                                    <label>Ja</label>
+                                    <textarea  rows="1" cols="25" id="checkfield" type="text"></textarea>
+                                </div>
+                                <p>
+                                    <input type="radio" name="radio-2">
+                                    <label>Nee</label>
+                                </p>
                             </div>
                         </div>
                         <div class="question"><p>Heeft u momenteel een infectie of overdraagbare besmettelijke aandoening?</p>
                             <div class="checkboxes">
-                                <div><input type="checkbox"> Ja</div>
-                                <div><input type="checkbox"> Nee</div>
+                                <div class="question-answer">
+                                    <input id="radio" type="radio" name="radio-3">
+                                    <label>Ja</label>
+                                    <textarea  rows="1" cols="25" id="checkfield" type="text"></textarea>
+                                </div>
+                                <p>
+                                    <input type="radio" name="radio-3">
+                                    <label>Nee</label>
+                                </p>
                             </div>
                         </div>
                         <div class="question"><p>Bent u ergens alergisch voor?</p>
                             <div class="checkboxes">
-                                <div><input type="checkbox"> Ja</div>
-                                <div><input type="checkbox"> Nee</div>
+                                <div class="question-answer">
+                                    <input id="radio" type="radio" name="radio-4">
+                                    <label>Ja</label>
+                                    <textarea  rows="1" cols="25" id="checkfield" type="text"></textarea>
+                                </div>
+                                <p>
+                                    <input type="radio" name="radio-4">
+                                    <label>Nee</label>
+                                </p>
                             </div>
                         </div>
-                        <div class="question"><p>Wat denkt u dat de oorzaak is van uw huidige situatie/toestand?</p><input type="text"></div>
-                        <div class="question"><p>- Wat heeft u eraan gedaan?</p><input type="text"></div>
-                        <div class="question"><p>- Hoe effectief was dat?</p><input type="text"></div>
-                        <div class="question"><p>- Hoe kunnen wij u helpen?</p><input type="text"></div>
-                        <div class="question"><p>- Wat is voor u belangrijk tijdens het verblijf op deze afdeling?</p><input type="text"></div>
-                        <div class="question"><p>- Vind u het gemakkelijk om dingen te doen of te laten op advies van de arts of verpleegkundige?</p><input type="text"></div>
-                        <div class="question"><p>Wat moet u in de toekomst doen ter voorkoming van het weer ziek worden?</p><input type="text"></div>
+                        <div class="question"><p>Wat denkt u dat de oorzaak is van uw huidige situatie/toestand?</p><textarea  rows="1" cols="25" type="text"></textarea></div>
+                        <div class="question"><p>- Wat heeft u eraan gedaan?</p><textarea  rows="1" cols="25" type="text"></textarea></div>
+                        <div class="question"><p>- Hoe effectief was dat?</p><textarea  rows="1" cols="25" type="text"></textarea></div>
+                        <div class="question"><p>- Hoe kunnen wij u helpen?</p><textarea  rows="1" cols="25" type="text"></textarea></div>
+                        <div class="question"><p>- Wat is voor u belangrijk tijdens het verblijf op deze afdeling?</p><textarea  rows="1" cols="25" type="text"></textarea></div>
+                        <div class="question"><p>- Vind u het gemakkelijk om dingen te doen of te laten op advies van de arts of verpleegkundige?</p><textarea  rows="1" cols="25" type="text"></textarea></div>
+                        <div class="question"><p>Wat moet u in de toekomst doen ter voorkoming van het weer ziek worden?</p><textarea  rows="1" cols="25" type="text"></textarea></div>
 
+                        
                         <div class="observation">
                             <h2>Verpleegkundige observatie bij dit patroon</h2>
                             <div class="question"><div class="observe"><input type="checkbox"><p>Gezondheidszoekend gedrag</p></div></div>
