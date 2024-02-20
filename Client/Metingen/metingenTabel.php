900b8071ddc37592bd833b194f32acff23404b43
Rens van Rijt <rensrijt@gmail.com>
2024-02-20 11:33:38 +0100

    Verwijder POST request gedeelte voor tabel

--- Client/Metingen/metingenTabel.php
+++ Client/Metingen/metingenTabel.php
@@ -14,33 +14,6 @@ if (!isset($_SESSION['loggedin_id'])) {
 }
 
 $samenStellingen = DatabaseConnection::getConn()->query("SELECT id, type, uiterlijk FROM samenstelling")->fetch_all(MYSQLI_ASSOC);
-
-if ($_SERVER['REQUEST_METHOD'] == 'POST') {
-    $hartslag = $_POST['hartslag'];
-    $ademhaling = $_POST['ademhaling'];
-    $bloeddruk = $_POST['bloeddruk'];
-    $temperatuur = $_POST['temperatuur'];
-    $vochtinname = $_POST['vochtinname'];
-    $uitscheiding = $_POST['uitscheiding'];
-    $uitscheidingSamenstelling = $_POST['uitscheidingSamenstelling'];
-    $uitscheidingPlas = $_POST['uitscheidingPlas'];
-    $pijnschaal = $_POST['pijnschaal'];
-
-    $verzorgerregelid = DatabaseConnection::getConn()->query("SELECT id FROM verzorgerregel WHERE medewerkerid = $id")->fetch_array()[0];
-    $time = date("Y-m-d H:i:s");
-
-    $meting = DatabaseConnection::getConn()->prepare("INSERT INTO meting (verzorgerregelid, datumtijd, hartslag, ademhaling, bloeddruklaag, temperatuur, vochtinname, pijn, bloeddrukhoog) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)");
-    $meting->bind_param("isiiiiiii", $verzorgerregelid, $time, $hartslag, $ademhaling, $bloeddruk, $temperatuur, $vochtinname, $pijnschaal, $uitscheidingPlas);
-    $meting->execute();
-    $metingId = $meting->insert_id;
-
-    $metingUrine = DatabaseConnection::getConn()->prepare("INSERT INTO metingurine (metingid, datumtijd, hoeveelheid) VALUES (?, ?, ?)");
-    $metingUrine->bind_param("isi", $metingId, $time, $uitscheidingPlas);
-    $metingUrine->execute();
-
-    $metingUrineSamenstelling = DatabaseConnection::getConn()->prepare("INSERT INTO metingontlasting (metingid, samenstellingid, datumtijd) VALUES (?, ?, ?)");
-    $metingUrineSamenstelling->bind_param("iis", $metingId, $uitscheidingSamenstelling, $time);
-    $metingUrineSamenstelling->execute();
 }
 ?>
 <!doctype html>
