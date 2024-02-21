d28e0f721d69e5a3c17be604a832a2805f68d31f
Foxie <105858467+Foxiel@users.noreply.github.com>
2024-02-21 10:43:49 +0100

    Tabel css

---
+++ Client/Metingen/metingeninvullen.php
@@ -0,0 +1,272 @@
+<?php
+session_start();
+include '../../Database/DatabaseConnection.php';
+
+$id = $_GET['id'];
+$_SESSION['clientId'] = $_GET['id'];
+
+if (!isset($id)) {
+    header("Location: ../../index.php");
+}
+
+$client = DatabaseConnection::getConn()->prepare("SELECT * FROM client WHERE id = ?");
+$client->bind_param("i", $id);
+$client->execute();
+$client = $client->get_result()->fetch_assoc();
+
+if ($client == null) {
+    header("Location: ../../index.php");
+}
+
+if (!isset($_SESSION['loggedin_id'])) {
+    header("Location: ../../index.php");
+}
+
+$samenStellingen = DatabaseConnection::getConn()->query("SELECT id, type, uiterlijk FROM samenstelling")->fetch_all(MYSQLI_ASSOC);
+
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+    $hartslag = $_POST['hartslag'];
+    $ademhaling = $_POST['ademhaling'];
+    $bloeddruklaag = $_POST['bloeddruk'];
+    $bloeddrukhoog = $_POST['bloeddruk2'];
+    $temperatuur = $_POST['temperatuur'];
+    $vochtinname = $_POST['vochtinname'];
+    $uitscheiding = $_POST['uitscheiding'];
+    $uitscheidingStool = $_POST['uitscheiding2'];
+    $uitscheidingPlas = $_POST['uitscheidingPlas'];
+    $pijnschaal = $_POST['pijnschaal'];
+    $uitscheidingSamenstelling = $_POST['uitscheidingSamenstelling'];
+
+    $verzorgerregelid = DatabaseConnection::getConn()->query("SELECT id FROM verzorgerregel WHERE medewerkerid = $id")->fetch_array()[0];
+    $time = date("Y-m-d H:i:s");
+
+    $meting = DatabaseConnection::getConn()->prepare("INSERT INTO meting (verzorgerregelid, datumtijd, hartslag, ademhaling, bloeddruklaag, bloeddrukhoog, temperatuur, vochtinname, pijn) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)");
+    $meting->bind_param("isiiiiiii", $verzorgerregelid, $time, $hartslag, $ademhaling, $bloeddruklaag, $bloeddrukhoog, $temperatuur, $vochtinname, $pijnschaal);
+    $meting->execute();
+    $metingId = $meting->insert_id;
+
+    $metingUrine = DatabaseConnection::getConn()->prepare("INSERT INTO metingurine (metingid, datumtijd, hoeveelheid) VALUES (?, ?, ?)");
+    $metingUrine->bind_param("isi", $metingId, $time, $uitscheidingPlas);
+    $metingUrine->execute();
+
+    $metingUrineSamenstelling = DatabaseConnection::getConn()->prepare("INSERT INTO metingontlasting (metingid, samenstellingid, datumtijd) VALUES (?, ?, ?)");
+    $metingUrineSamenstelling->bind_param("iis", $metingId, $uitscheidingSamenstelling, $time);
+    $metingUrineSamenstelling->execute();
+}
+?>
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Metingen</title>
+    <link rel="stylesheet" href="metingen.css">
+</head>
+<body>
+<?php
+include_once '../../Includes/header.php';
+?>
+<div class="main">
+    <?php
+    include_once '../../Includes/sidebar.php';
+    ?>
+    <div class="main2">
+        <div class="btns">
+            <?php echo '<a href="metingeninvullen.php?id='.$id.'"><button type="button" class="MetingenInvul">Metingen invullen</button></a>'; ?>
+            <?php echo '<a href="metingen.php?id='.$id.'"><button type="button" class="MetingenTabel">Metingen bekijken</button></a>'; ?>
+        </div>
+        <form id="patientForm" method="POST">
+            <!-- metingen -->
+            <label for="Hartslag">Hartslag:</label>
+            <input type="number" id="hartslag" name="hartslag" placeholder="slagen per minuut" required min="0" max="200"> <!-- o tot 200 -->
+
+            <label for="Ademhaling">Ademhaling:</label>
+            <input type="number" id="ademhaling" name="ademhaling" placeholder="tussen 0 , 80" required min="0" max="80"> <!-- o tot 80 -->
+
+            <div class="bloeddrukken">
+                <div class="bloeddruk-lengte">
+                    <label for="Bloed druk">Bloeddruk Laag:</label>
+                    <input type="text" id="bloeddruk" name="bloeddruk" placeholder="Laag" required min="0" max="140"> <!-- o tot 140 -->
+                </div>
+                <div class="bloeddruk-lengte">
+                    <label for="Bloed druk">Bloeddruk Hoog:</label>
+                    <input type="text" id="bloeddruk2" name="bloeddruk2" placeholder="Hoog" required min="0" max="140"> <!-- o tot 140 -->
+                </div>
+            </div>
+
+            <label for="Temperatuur">Temperatuur:</label>
+            <input type="number" id="temperatuur" name="temperatuur" placeholder="b.v.b, 37.9" required min="34" max="42"> <!-- 34° tot 42° -->
+
+            <label for="Vochtinname">Vochtinname:</label>
+            <input type="number" id="vochtinname" name="vochtinname" placeholder="Invoeren in aantal milliliters" required min="0" max="5000"> <!-- o tot 5000 -->
+
+            <div class="Uitscheidingen">
+                <div class="Uitscheiding2">
+                    <label for="Uitscheiding">Uitscheiding:</label>
+                    <input type="number" id="uitscheiding" name="uitscheiding" placeholder="Invoeren in frequentie per dag" >
+                </div>
+                <div class="Uitscheiding2">
+                    <label for="Uitscheiding">Uitscheiding bristol stool chart:</label>
+                    <input type="number" id="uitscheiding2" name="uitscheiding2" placeholder="Invoeren in frequentie per dag" >
+                </div>
+            </div>
+
+            <label for="Uitscheidingplas">Uitscheiding plas:</label>
+            <input type="number" id="uitscheidingplas" name="uitscheidingPlas" placeholder="Invoeren in aantal milliliters" required>
+
+            <label for="UitscheidingSamenstelling">Uitscheiding samenstelling:</label>
+            <select id="uitscheidingSamenstelling" name="uitscheidingSamenstelling" required>
+                <?php
+                foreach ($samenStellingen as $samenStelling) {
+                    echo "<option value='$samenStelling[id]'>$samenStelling[uiterlijk]</option>";
+                }
+                ?>
+            </select>
+
+            <label for="Pijnschaal">Pijnschaal:</label>
+            <input type="number" id="pijnschaal" name="pijnschaal" placeholder="van 1 tot 10" required min="1" max="10">
+            <br/><button class="metingButton" type="button" onclick="submit()">Submit</button>
+        </form>
+
+    </div>
+    <script>
+        const submitButton = document.getElementById('submitButton');
+        const hartslag = document.getElementById('hartslag');
+        const ademhaling = document.getElementById('ademhaling');
+        const bloeddruk = document.getElementById('bloeddruk');
+        const bloeddruk2 = document.getElementById('bloeddruk2');
+        const temperatuur = document.getElementById('temperatuur');
+        const vochtinname = document.getElementById('vochtinname');
+        const uitscheiding = document.getElementById('uitscheiding');
+        const uitscheiding2 = document.getElementById('uitscheiding2');
+        const uitscheidingplas = document.getElementById('uitscheidingplas');
+        const pijnschaal = document.getElementById('pijnschaal');
+
+
+        hartslag.addEventListener('input', hartslagUpdate);
+        ademhaling.addEventListener('input', ademHalingUpdate);
+        bloeddruk.addEventListener('input', bloeddrukUpdate);
+        bloeddruk2.addEventListener('input', bloeddruk2Update);
+        temperatuur.addEventListener('input', temperatuurUpdate);
+        vochtinname.addEventListener('input', vochtinnameUpdate);
+        uitscheiding.addEventListener('input', uitscheidingUpdate);
+        uitscheiding2.addEventListener('input', uitscheiding2Update);
+        uitscheidingplas.addEventListener('input', uitscheidingplasUpdate);
+        pijnschaal.addEventListener('input', pijnschaalUpdate);
+
+        function hartslagUpdate() {
+            if (hartslag) {
+                if (hartslag.value < 0 || hartslag.value > 200) {
+                    hartslag.style.border = '5px solid red';
+                }else {
+                    hartslag.style.border = '1px solid black';
+                }
+            }else {
+                console.log("error");
+            }
+        }
+
+        function ademHalingUpdate() {
+            if (ademhaling) {
+                if (ademhaling.value < 0 || ademhaling.value > 80) {
+                    ademhaling.style.border = '5px solid red';
+                }else {
+                    ademhaling.style.border = '1px solid black';
+                }
+            }else{
+                console.log('error');
+            }
+        }
+        function bloeddrukUpdate() {
+            if (bloeddruk) {
+                if (bloeddruk.value < 0 || bloeddruk.value > 140) {
+                    bloeddruk.style.border = '5px solid red';
+                }else {
+                    bloeddruk.style.border = '1px solid black';
+                }
+            }else{
+                console.log('error');
+            }
+        }
+        function bloeddruk2Update() {
+            if (bloeddruk2) {
+                if (bloeddruk2.value < 0 || bloeddruk2.value > 140) {
+                    bloeddruk2.style.border = '5px solid red';
+                }else {
+                    bloeddruk2.style.border = '1px solid black';
+                }
+            }else{
+                console.log('error');
+            }
+        }
+        function temperatuurUpdate() {
+            if (temperatuur) {
+                if (temperatuur.value < 34 || temperatuur.value > 42) {
+                    temperatuur.style.border = '5px solid red';
+                }else {
+                    temperatuur.style.border = '1px solid black';
+                }
+            }else{
+                console.log('error');
+            }
+        }
+        function vochtinnameUpdate() {
+            if (vochtinname) {
+                if (vochtinname.value < 0 || vochtinname.value > 5000) {
+                    vochtinname.style.border = '5px solid red';
+                }else {
+                    vochtinname.style.border = '1px solid black';
+                }
+            }else{
+                console.log('error');
+            }
+        }
+        function uitscheidingUpdate() {
+            if (uitscheiding) {
+                if (!uitscheiding.value) {
+                    uitscheiding.style.border = '5px solid red';
+                }else {
+                    uitscheiding.style.border = '1px solid black';
+                }
+            }else{
+                console.log('error');
+            }
+        }
+        function uitscheiding2Update() {
+            if (uitscheiding2) {
+                if (uitscheiding2.value < 1 || uitscheiding2.value > 7) {
+                    uitscheiding2.style.border = '5px solid red';
+                }else {
+                    uitscheiding2.style.border = '1px solid black';
+                }
+            }else{
+                console.log('error');
+            }
+        }
+        function uitscheidingplasUpdate() {
+            if (uitscheidingplas) {
+                if (!uitscheidingplas.value) {
+                    uitscheidingplas.style.border = '5px solid red';
+                }else {
+                    uitscheidingplas.style.border = '1px solid black';
+                }
+            }else{
+                console.log('error');
+            }
+        }
+        function pijnschaalUpdate() {
+            if (pijnschaal) {
+                if (pijnschaal.value < 0 || pijnschaal.value > 10) {
+                    pijnschaal.style.border = '5px solid red';
+                }else {
+                    pijnschaal.style.border = '1px solid black';
+                }
+            }else{
+                console.log('error');
+            }
+        }
+    </script>
+</body>
+</html>
---
+++ Functions/MetingenFunctions.php
@@ -0,0 +1,90 @@
+<?php
+
+function getMeting($metingtijden){
+    $metingen = [];
+    $tijden = [];
+    foreach ($metingtijden as $metingtijd) {
+        $datumtijd = $metingtijd['datumtijd'];
+        $metingid = $metingtijd['id'];
+        $tijd = date('H:i', strtotime($datumtijd));
+        $tijden[] = $tijd;
+        $query = "(SELECT
+            'hartslag' AS meting,
+            MAX(CASE WHEN datumtijd = '$datumtijd' THEN hartslag ELSE null END) AS '$tijd'
+            FROM meting
+            WHERE verzorgerregelid = 1)
+
+            UNION
+            
+            (SELECT
+            'ademhaling' AS meting,
+            MAX(CASE WHEN datumtijd = '$datumtijd' THEN ademhaling ELSE null END) AS '$tijd'
+            FROM meting
+            WHERE verzorgerregelid = 1)
+
+            UNION
+            
+            (SELECT
+            'bloeddruklaag' AS meting,
+            MAX(CASE WHEN datumtijd = '$datumtijd' THEN bloeddruklaag ELSE null END) AS '$tijd'
+            FROM meting
+            WHERE verzorgerregelid = 1)
+
+            UNION
+            
+            (SELECT
+            'temperatuur' AS meting,
+            MAX(CASE WHEN datumtijd = '$datumtijd' THEN temperatuur ELSE null END) AS '$tijd'
+            FROM meting
+            WHERE verzorgerregelid = 1)
+
+            UNION
+            
+            (SELECT
+            'vochtinname' AS meting,
+            MAX(CASE WHEN datumtijd = '$datumtijd' THEN vochtinname ELSE null END) AS '$tijd'
+            FROM meting
+            WHERE verzorgerregelid = 1)
+
+            UNION
+            
+            (SELECT
+            'pijn' AS meting,
+            MAX(CASE WHEN datumtijd = '$datumtijd' THEN pijn ELSE null END) AS '$tijd'
+            FROM meting
+            WHERE verzorgerregelid = 1)
+
+            UNION
+            
+            (SELECT
+            'bloeddrukhoog' AS meting,
+            MAX(CASE WHEN datumtijd = '$datumtijd' THEN bloeddrukhoog ELSE null END) AS '$tijd'
+            FROM meting
+            WHERE verzorgerregelid = 1)
+
+            UNION
+            
+            (SELECT
+            'samenstelling' AS metingontlasting,
+            MAX(CASE WHEN datumtijd = '$datumtijd' THEN samenstellingid ELSE null END) AS '$tijd'
+            FROM metingontlasting
+            WHERE metingid = $metingid)
+
+            UNION
+            
+            (SELECT
+            'hoeveelheid' AS metingurine,
+            MAX(CASE WHEN datumtijd = '$datumtijd' THEN hoeveelheid ELSE null END) AS '$tijd'
+            FROM metingurine
+            WHERE metingid = $metingid)";
+        $result = DatabaseConnection::getConn()->query($query);
+        array_push($metingen, $result->fetch_all(MYSQLI_ASSOC));
+    }
+    $arrays = [];
+    array_push($arrays, $tijden);
+    array_push($arrays, $metingen);
+    return $arrays;
+}
+
+
+
