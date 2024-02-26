3677b3f93a022bcef73a128940ad7edbb2797ffd
Mike <mike.van.pol@student.gildeopleidingen.nl>
2024-02-26 14:10:16 +0100

    Fix typefout cognitie

--- SQLscripts/dpd_testdata08_alterColumn.sql
+++ SQLscripts/dpd_testdata08_alterColumn.sql
@@ -1,2 +1,3 @@
 UPDATE `patroontype` SET `type` = 'Zelfbelevingspatroon' WHERE `patroontype`.`id` = 7; 
-UPDATE `patroontype` SET `type` = 'Uitscheidingspatroon' WHERE `patroontype`.`id` = 3; 
+UPDATE `patroontype` SET `type` = 'Uitscheidingspatroon' WHERE `patroontype`.`id` = 3;
+UPDATE `patroontype` SET `type` = 'Cognitie- en waarnemingspatroon' WHERE `patroontype`.`id` = 6; 
