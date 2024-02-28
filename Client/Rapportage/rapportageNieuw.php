bf7ac0a4c8755fbbfff150ac45e0026a1a0c747e
Creekie <hi@creekie.me>
2024-02-28 10:49:38 +0100

    Stuur naar de nieuwe rapportage

--- Client/Rapportage/rapportageNieuw.php
+++ Client/Rapportage/rapportageNieuw.php
@@ -32,35 +32,4 @@ $rapportage->bind_param("iss", $verzorgerregel, $tijd, $rapport);
 $rapportage->execute();
 $rapportage = $rapportage->insert_id;
 
-?>
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="Stylesheet" href="rapportage.css">
-    <title>Rapportage van <?= $client['naam'] ?></title>
-</head>
-<body>
-<div class="main">
-        <?php
-        include '../../Includes/header.php';
-        ?>
-
-        <?php
-        include '../../Includes/sidebar.php';
-        ?>
-
-        <div class="content">
-                    <form method="POST">
-                        <div class="rapportage">
-                            <textarea name="rapportage" id="rapportage" placeholder="Rapportage" style="width: 100%; height: 100%; box-sizing: border-box;"></textarea>
-                            <button class="rapportageButton" type="button" onclick="submit()">Submit</button>
-                        </div>
-
-                    </form>
-        </div>
-    </div>
-
-</body>
-</html>
+header("Location: rapportageAanpassen.php?id=" . $rapportage);
