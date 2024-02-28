3b6cc48256ef023cf359491143eb6db5305a4f7a
Fayaaz Abdoel <90307474+ReddAstray@users.noreply.github.com>
2024-02-28 13:47:03 +0100

    small fix

--- Client/Patiëntgegevens/patiëntgegevens.php
+++ Client/Patiëntgegevens/patiëntgegevens.php
@@ -38,7 +38,7 @@ foreach ($clientRelations as $relation) {
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="Stylesheet" href="overzicht.css">
+    <link rel="Stylesheet" href="patiëntgegevens.css">
     <title>Overzicht van <?= $client['naam'] ?></title>
 </head>
     <body>
