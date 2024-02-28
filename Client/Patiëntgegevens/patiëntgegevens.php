fa7150f92e1db9f9c438df85369fe28baf6c58f4
Fayaaz Abdoel <90307474+ReddAstray@users.noreply.github.com>
2024-02-28 12:46:24 +0100

    merge main

--- Client/Patiëntgegevens/patiëntgegevens.php
+++ Client/Patiëntgegevens/patiëntgegevens.php
@@ -14,11 +14,16 @@ $client->bind_param("i", $id);
 $client->execute();
 $client = $client->get_result()->fetch_assoc();
 
+if ($client == null) {
+    header("Location: ../client.php");
+}
+
 $clientRelations = DatabaseConnection::getConn()->prepare("SELECT * FROM verzorgerregel WHERE clientid = ?");
 $clientRelations->bind_param("i", $id);
 $clientRelations->execute();
 $clientRelations = $clientRelations->get_result()->fetch_all(MYSQLI_ASSOC);
 
+$verzorgerNamen = [];
 $verzorgers = [];
 foreach ($clientRelations as $relation) {
     $verzorger = DatabaseConnection::getConn()->prepare("SELECT * FROM medewerker WHERE id = ?");
@@ -28,83 +33,79 @@ foreach ($clientRelations as $relation) {
     array_push($verzorgers, $verzorger);
 }
 ?>
-
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="Stylesheet" href="patiëntgegevens.css">
-    <title>Gegevens van <?= $client['naam'] ?></title>
+    <link rel="Stylesheet" href="overzicht.css">
+    <title>Overzicht van <?= $client['naam'] ?></title>
 </head>
-<body>
-<div class="main">
-    <?php
-    include '../../Includes/header.php';
-    ?>
+    <body>
+        <div class="main">
+            <?php
+            include_once '../../Includes/header.php';
+            include_once '../../Includes/sidebar.php';
+            ?>
 
-    <?php
-    include '../../Includes/sidebar.php';
-    ?>
-
-    <div class="content">
-        <div class="patiëntgegevens">
-            <div class="patiëntgegevens-content">
-                <div class="text">
-                    <strong>Geslacht</strong>
-                    <p><?= $client['geslacht'] ?></p>
-                </div>
-                <div class="text">
-                    <strong>Geboortedatum</strong>
-                    <p><?= $client['geboortedatum'] ?></p>
-                </div>
-                <div class="text">
-                    <strong>Adres</strong>
-                    <p><?= $client['adres'] ?></p>
-                </div>
-                <div class="text">
-                    <strong>Postcode</strong>
-                    <p><?= $client['postcode'] ?></p>
-                </div>
-                <div class="text">
-                    <strong>Woonplaats</strong>
-                    <p><?= $client['woonplaats'] ?></p>
+        <div class="content">
+                <div class="overzicht">
+                    <div class="overzicht-content">
+                        <div class="text">
+                            <strong>Geslacht</strong>
+                            <p><?= $client['geslacht'] ?></p>
+                        </div>
+                        <div class="text">
+                            <strong>Geboortedatum</strong>
+                            <p><?= date_create($client['geboortedatum'])->format('d-m-Y') ?></p>
+                        </div>
+                        <div class="text">
+                            <strong>Adres</strong>
+                            <p><?= $client['adres'] ?></p>
+                        </div>
+                        <div class="text">
+                            <strong>Postcode</strong>
+                            <p><?= $client['postcode'] ?></p>
+                        </div>
+                        <div class="text">
+                            <strong>Woonplaats</strong>
+                            <p><?= $client['woonplaats'] ?></p>
+                        </div>
+                    </div>
+                    <br>
+                    <div class="overzicht-content">
+                        <div class="text">
+                            <strong>Telefoonnummer</strong>
+                            <p><?= $client['telefoonnummer'] ?></p>
+                        </div>
+                        <div class="text">
+                            <strong>E-mail</strong>
+                            <p><?= $client['email'] ?></p>
+                        </div>
+                        <div class="text">
+                            <strong>Afdeling</strong>
+                            <p><?= $client['afdeling'] ?></p>
+                        </div>
+                        <div class="text">
+                            <strong>Burgelijke staat</strong>
+                            <p><?= $client['burgelijkestaat'] ?></p>
+                        </div>
+                        <div class="text">
+                            <strong>Nationaliteit</strong>
+                            <p><?= $client['nationaliteit'] ?></p>
+                        </div>
+                        <div class="text">
+                            <a href="verzorgers.php?id=<?= $_GET['id']?>">
+                                <strong>Verzorger(s)</strong>
+                            </a>
+                            <?php foreach ($verzorgers as $verzorger) { ?>
+                                <p><?= $verzorger['naam'] ?></p>
+                            <?php } ?>
+                        </div>
+                    </div>
                 </div>
             </div>
-            <br>
-            <div class="patiëntgegevens-content">
-                <div class="text">
-                    <strong>Telefoonnummer</strong>
-                    <p><?= $client['telefoonnummer'] ?></p>
-                </div>
-                <div class="text">
-                    <strong>E-mail</strong>
-                    <p><?= $client['email'] ?></p>
-                </div>
-                <div class="text">
-                    <strong>Afdeling</strong>
-                    <p><?= $client['afdeling'] ?></p>
-                </div>
-                <div class="text">
-                    <strong>Burgelijke staat</strong>
-                    <p><?= $client['burgelijkestaat'] ?></p>
-                </div>
-                <div class="text">
-                    <strong>Nationaliteit</strong>
-                    <p><?= $client['nationaliteit'] ?></p>
-                </div>
-                <div class="text">
-                    <a href="verzorgers.php?id=<?= $_GET['id']?>">
-                        <strong>Verzorger(s)</strong>
-                    </a>
-                    <?php foreach ($verzorgers as $verzorger) { ?>
-                        <p><?= $verzorger['naam'] ?></p>
-                    <?php } ?>
-                </div>
-            </div>
-        </div>
     </div>
-</div>
 
 </body>
 </html>
