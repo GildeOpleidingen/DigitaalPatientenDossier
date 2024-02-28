b4e444b0fa4faa453a569f26005ee4c21eaae9c2
Mike <mike.van.pol@student.gildeopleidingen.nl>
2024-02-28 10:53:50 +0100

    Fix typefouten en add missende kolom

--- SQLscripts/dpd_testdata09_alterColumn.sql
+++ SQLscripts/dpd_testdata09_alterColumn.sql
@@ -1,1 +1,4 @@
 ALTER TABLE `patroon10stressverwerking` ADD `reactie_anders` TINYTEXT NULL DEFAULT NULL AFTER `reactie_spanningen`; 
+ALTER TABLE `patroon06cognitiewaarneming` CHANGE `moete_met_zien_wat` `moeite_met_zien_wat` TINYTEXT CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL; 
+ALTER TABLE `patroon06cognitiewaarneming` CHANGE `moete_met_zien` `moeite_met_zien` TINYINT(1) NOT NULL; 
+ALTER TABLE `patroon10stressverwerking` CHANGE `agressiief` `agressief` TINYINT(1) NOT NULL; 
