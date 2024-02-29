63969d2b4e1a83a7bd8decb19c541aaa9bcbf1ba
Flamex <105858527+Flamexyy@users.noreply.github.com>
2024-02-29 11:01:51 +0100

    patronen pagina's responsiveness

--- Client/Anamnese/patroon03.php
+++ Client/Anamnese/patroon03.php
@@ -38,7 +38,6 @@ if (isset($_REQUEST['navbutton'])) {
         <?php
         include '../../Includes/sidebar.php';
         ?>
-    <div class="main-content">
         <div class="content">
             <div class="form-content">
             <div class="pages">3 Uitscheidingspatroon</div>
@@ -143,9 +142,7 @@ if (isset($_REQUEST['navbutton'])) {
                 </div>
             </div>
         </div>
+        </div>
         </form>
-    </div>
-    </div>
-
 </body>
 </html>
--- Client/Anamnese/patroon06.php
+++ Client/Anamnese/patroon06.php
@@ -38,7 +38,6 @@ if (isset($_REQUEST['navbutton'])) {
         <?php
         include '../../Includes/sidebar.php';
         ?>
-    <div class="main-content">
         <div class="content">
             <div class="form-content">
             <div class="pages">6 Cognitie- en waarnemingspatroon</div>
@@ -250,9 +249,7 @@ if (isset($_REQUEST['navbutton'])) {
                 </div>
             </div>
         </div>
+        </div>
         </form>
-    </div>
-    </div>
-
 </body>
 </html>
