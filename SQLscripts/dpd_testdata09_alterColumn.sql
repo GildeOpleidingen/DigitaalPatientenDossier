1e3439c4f56bb648e1636f4286911354a0b88481
Mike <mike.van.pol@student.gildeopleidingen.nl>
2024-03-04 11:55:43 +0100

    Fix typefouten in database

--- SQLscripts/dpd_testdata09_alterColumn.sql
+++ SQLscripts/dpd_testdata09_alterColumn.sql
@@ -11,3 +11,7 @@ ALTER TABLE `patroon06cognitiewaarneming` ADD `ziet_dingen` TINYINT NOT NULL AFT
 ALTER TABLE `patroon06cognitiewaarneming` CHANGE `moete_met_zien` `moeite_met_zien` TINYINT(1) NOT NULL; 
 ALTER TABLE `patroon06cognitiewaarneming` CHANGE `moete_met_zien_wat` `moeite_met_zien_wat` TINYTEXT CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL; 
 ALTER TABLE `patroon06cognitiewaarneming` CHANGE `gebruikt_middelen_softdrugs` `gebruikt_middelen_softdrugs_welke` VARCHAR(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL, CHANGE `gebruikt_middelen_harddrugs` `gebruikt_middelen_harddrugs_welke` VARCHAR(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL, CHANGE `gebruikt_middelen_alcohol` `gebruikt_middelen_alcohol_welke` VARCHAR(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL, CHANGE `gebruikt_middelen_anders` `gebruikt_middelen_anders_welke` VARCHAR(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL; 
+
+ALTER TABLE `patroon10stressverwerking` CHANGE `angsig_paniek` `angstig_paniek` TINYINT(1) NOT NULL; 
+ALTER TABLE `patroon10stressverwerking` CHANGE `angsig_paniek_actie` `angstig_paniek_actie` TINYTEXT CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL; 
+ALTER TABLE `patroon10stressverwerking` CHANGE `angsig_paniek_lukt_voorkomen` `angstig_paniek_lukt_voorkomen` TINYINT(1) NOT NULL; 
