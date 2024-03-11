2171ae75c8cfc3e474e952ab75ded88cc15933ee
Mike <mike.van.pol@student.gildeopleidingen.nl>
2024-03-11 11:53:43 +0100

    Add rol kolom in database en verander medewerker rol van ids 1-3

--- SQLscripts/dpd_testdata11_alterColumn.sql
+++ SQLscripts/dpd_testdata11_alterColumn.sql
@@ -1,2 +1,6 @@
 ALTER TABLE `medewerker` CHANGE `wachtwoord` `wachtwoord` VARCHAR(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL; 
 ALTER TABLE `medewerker` DROP `salt`;
+ALTER TABLE `medewerker` ADD `rol` VARCHAR(50) NOT NULL AFTER `grens_assistent`;
+UPDATE `medewerker` SET `rol` = 'medewerker' WHERE `medewerker`.`id` = 1;
+UPDATE `medewerker` SET `rol` = 'medewerker' WHERE `medewerker`.`id` = 2;
+UPDATE `medewerker` SET `rol` = 'medewerker' WHERE `medewerker`.`id` = 3;
