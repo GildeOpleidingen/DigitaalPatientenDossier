2ff105448f9cc4f4f81dcefddb05c0e24ea14396
Mike <mike.van.pol@student.gildeopleidingen.nl>
2024-03-12 14:32:41 +0100

    Fix redirect naar index.php

--- Medewerker/Register/register.php
+++ Medewerker/Register/register.php
@@ -2,7 +2,7 @@
 session_start();
 
 if (!isset($_SESSION['loggedin_id']) || $_SESSION['rol'] != "beheerder") {
-    header("Location: ../index.php");
+    header("Location: ../../index.php");
 }
 
 include '../../Database/DatabaseConnection.php';
