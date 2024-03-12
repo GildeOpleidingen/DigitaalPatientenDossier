0506375d71ff38eb417c07a772433c20c0436068
Piotr <piotr.poplawski@student.gildeopleidingen.nl>
2024-03-12 14:30:16 +0100

    #32 patroon05 af

--- Client/Anamnese/patroon05.php
+++ Client/Anamnese/patroon05.php
@@ -8,22 +8,138 @@ $antwoorden = getPatternAnswers($_SESSION['clientId'], 5);
 $boolArrayInslaapmiddel = str_split($antwoorden['gebruik_inslaapmiddel_welke']);
 $boolArrayObservatie = str_split($antwoorden['observatie']);
 
+$medewerkerId = $_SESSION['loggedin_id'];
+
 if (isset($_REQUEST['navbutton'])) {
-    //TODO: hier actie om data op te slaan in database.
-    switch($_REQUEST['navbutton']) {
+    $verandering_inslaaptijd = $_POST['verandering_inslaaptijd'];
+    $verandering_inslaaptijd_blijktuit = $_POST['verandering_inslaaptijd_blijktuit'];
+    $verandering_kwaliteit_slapen = ($_POST['verandering_kwaliteit_slapen']);
+    $verandering_kwaliteit_slapen_blijktuit = ($_POST['verandering_kwaliteit_slapen_blijktuit']);
+    $gebruik_inslaapmiddel = $_POST['gebruik_inslaapmiddel'];
+    $gebruik_inslaapmiddel_welke = $_POST['gebruik_inslaapmiddel_welke'] ?? "";
+    $gebruik_inslaapmiddel_anders = $_POST['gebruik_inslaapmiddel_anders'] ?? "";
+    $slaapduur = $_POST['slaapduur'];
+    $uitgerust_wakker = $_POST['uitgerust_wakker'];
+    $dromen_nachtmerries = $_POST['dromen_nachtmerries'];
+    $rustperiodes_overdag = $_POST['rustperiodes_overdag'];
+    $gemakkelijk_ontspannen = $_POST['gemakkelijk_ontspannen'];
+    // array van checkboxes van observatie tab
+    $observatie = array(!empty($_POST['observatie1']));
+
+    $observatie = convertBoolArrayToString($observatie);
+
+    $result = DatabaseConnection::getConn()->prepare("
+                    SELECT vl.id
+                    from vragenlijst vl
+                    left join verzorgerregel on verzorgerregel.id = vl.verzorgerregelid
+                    where verzorgerregel.clientid = ?");
+    $result->bind_param("i", $_SESSION['clientId']);
+    $result->execute();
+    $result = $result->get_result()->fetch_assoc();
+
+    if ($result != null){
+        $vragenlijstId = $result['id'];
+    } else {
+        $sql2 = DatabaseConnection::getConn()->prepare("INSERT INTO `vragenlijst`(`verzorgerregelid`)
+            VALUES ((SELECT id
+            FROM verzorgerregel
+            WHERE clientid = ?
+            AND medewerkerid = ?))");
+        $sql2->bind_param("ii", $_SESSION['clientId'] ,$medewerkerId);
+        $sql2->execute();
+        $sql2 = $sql2->get_result();
+
+        $result = DatabaseConnection::getConn()->prepare("SELECT vl.id
+                    from vragenlijst vl
+                    left join verzorgerregel on verzorgerregel.id = vl.verzorgerregelid
+                    where verzorgerregel.clientid = ?");
+        $result->bind_param("i", $_SESSION['clientId']);
+        $result->execute();
+        $result = $result->get_result()->fetch_assoc();
+
+        $vragenlijstId=$result['id'];
+    }
+
+    // kijken of patroon02 bestaat door te kijken naar vragenlijst id
+    $result = DatabaseConnection::getConn()->prepare("
+                    SELECT p.id
+                    FROM patroon05slaaprust p
+                    WHERE p.vragenlijstid =  ?");
+    $result->bind_param("i", $vragenlijstId);
+    $result->execute();
+    $result = $result->get_result()->fetch_assoc();
+
+    if ($result != null) {
+        //update
+        $result1 = DatabaseConnection::getConn()->prepare("UPDATE `patroon05slaaprust` SET
+        `verandering_inslaaptijd`='$verandering_inslaaptijd',
+        `verandering_inslaaptijd_blijktuit`='$verandering_inslaaptijd_blijktuit',
+        `verandering_kwaliteit_slapen`='$verandering_kwaliteit_slapen',
+        `verandering_kwaliteit_slapen_blijktuit`='$verandering_kwaliteit_slapen_blijktuit',
+        `gebruik_inslaapmiddel`='$gebruik_inslaapmiddel',
+        `gebruik_inslaapmiddel_welke`= '$gebruik_inslaapmiddel_welke',
+        `gebruik_inslaapmiddel_anders`='$gebruik_inslaapmiddel_anders',
+        `slaapduur`='$slaapduur',
+        `uitgerust_wakker`='$uitgerust_wakker',
+        `dromen_nachtmerries`='$dromen_nachtmerries',
+        `rustperiodes_overdag`='$rustperiodes_overdag',
+        `gemakkelijk_ontspannen`='$gemakkelijk_ontspannen',
+        `observatie`='$observatie'
+           WHERE `vragenlijstid`=?");
+        $result1->bind_param("i", $vragenlijstId);
+        $result1->execute();
+        $result1 = $result1->get_result();
+
+    }else{
+        //hier insert je alle data in patroon02
+
+        $result2 = DatabaseConnection::getConn()->prepare( "INSERT INTO `patroon05slaaprust`(
+                `vragenlijstid`,
+                `verandering_inslaaptijd`,
+                `verandering_inslaaptijd_blijktuit`,
+                `verandering_kwaliteit_slapen`,
+                `verandering_kwaliteit_slapen_blijktuit`,
+                `gebruik_inslaapmiddel`,
+                `gebruik_inslaapmiddel_welke`,
+                `gebruik_inslaapmiddel_anders`,
+                `slaapduur`,
+                `uitgerust_wakker`,
+                `dromen_nachtmerries`,
+                `rustperiodes_overdag`,
+                `gemakkelijk_ontspannen`,
+                `observatie`)
+            VALUES (
+                    ?,
+                    ?,
+                    ?,
+                    ?,
+                    ?,
+                    ?,
+                    ?,
+                    ?,
+                    ?,
+                    ?,
+                    ?,
+                    ?,
+                    ?,
+                    ?)");
+        $result2->bind_param("iisisisidiiiis", $vragenlijstId, $verandering_inslaaptijd,$verandering_inslaaptijd_blijktuit,$verandering_kwaliteit_slapen,$verandering_kwaliteit_slapen_blijktuit,$gebruik_inslaapmiddel,$gebruik_inslaapmiddel_welke,$gebruik_inslaapmiddel_anders,$slaapduur,$uitgerust_wakker,$dromen_nachtmerries,$rustperiodes_overdag,$gemakkelijk_ontspannen, $observatie);
+        $result2->execute();
+        $result2 = $result2->get_result();
+
+    }
+    switch ($_REQUEST['navbutton']) {
         case 'next': //action for next here
             header('Location: patroon06.php');
             break;
-    
+
         case 'prev': //action for previous here
             header('Location: patroon04.php');
             break;
     }
     exit;
 }
-
 ?>
-
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -35,7 +151,7 @@ if (isset($_REQUEST['navbutton'])) {
 <body>
     <form action="" method="post">
     <div class="main">
-        <?php
+<?php
         include '../../Includes/header.php';
         include '../../Includes/sidebar.php';
         ?>
@@ -142,7 +258,7 @@ if (isset($_REQUEST['navbutton'])) {
 
                         <div class="observation">
                             <h2>Verpleegkundige observatie bij dit patroon</h2>
-                            <div class="question"><div class="observe"><input type="checkbox" <?= $boolArrayObservatie[0] ? "checked" : "" ?>><p>Verstoord slaap- en rustpatroon</p></div></div>
+                            <div class="question"><div class="observe"><input type="checkbox" <?= $boolArrayObservatie[0] ? "checked" : "" ?> name="observatie1"><p>Verstoord slaap- en rustpatroon</p></div></div>
                         </div>
                     </div>
                 </div>
