88dc9ad17a806b92928db3b1b494604057f921f2
Milan <milanensink01@gmail.com>
2024-03-14 13:16:59 +0100

    Forgot patroon01 d;

--- Client/Anamnese/patroon01.php
+++ Client/Anamnese/patroon01.php
@@ -2,12 +2,14 @@
 session_start();
 include '../../Database/DatabaseConnection.php';
 include '../../Functions/Functions.php';
+include '../../Functions/AnamneseFunctions.php';
 
 $antwoorden = getPatternAnswers($_SESSION['clientId'], 1);
 
 $boolArrayObservatie = str_split($antwoorden['observatie']);
 //id van de medewerker
 $medewerker_id = $_SESSION['loggedin_id'];
+$clientId = $_SESSION['clientId'];
 
 if (isset($_REQUEST['navbutton'])) {
     $algemene_gezondheid = $_POST['algemene_gezondheid'];
@@ -32,50 +34,15 @@ if (isset($_REQUEST['navbutton'])) {
 
     $observatie = convertBoolArrayToString($observatie);
 
-    $result = DatabaseConnection::getConn()->prepare("
-                    SELECT vl.id
-                    from vragenlijst vl
-                    left join verzorgerregel on verzorgerregel.id = vl.verzorgerregelid
-                    where verzorgerregel.clientid = ?");
-    $result->bind_param("i", $_SESSION['clientId']);
-    $result->execute();
-    $result = $result->get_result()->fetch_assoc();
-
-
+    $result = getVragenlijstId($clientId);
     if ($result != null){
-        $vragenlijst_id = $result['id'];
-
+        $vragenlijstId = $result;
     } else {
-
-        $sql2 = DatabaseConnection::getConn()->prepare("INSERT INTO `vragenlijst`(`verzorgerregelid`)
-            VALUES ((SELECT id
-            FROM verzorgerregel
-            WHERE clientid = ?
-            AND medewerkerid = ?))");
-        $sql2->bind_param("ii", $_SESSION['clientId'] ,$medewerker_id);
-        $sql2->execute();
-        $sql2 = $sql2->get_result();
-
-
-        $result = DatabaseConnection::getConn()->prepare("SELECT vl.id
-                    from vragenlijst vl
-                    left join verzorgerregel on verzorgerregel.id = vl.verzorgerregelid
-                    where verzorgerregel.clientid = ?");
-        $result->bind_param("i", $_SESSION['clientId']);
-        $result->execute();
-        $result = $result->get_result()->fetch_assoc();
-
-        $vragenlijst_id=$result['id'];
-
+        $result = insertVragenlijst($_SESSION['clientId'], $medewerkerId);
+        $vragenlijstId = $result;
     }
 
-    // kijken of patroon02 bestaat door te kijken naar vragenlijst id
-    $result = DatabaseConnection::getConn()->prepare("SELECT p.id
-                    from patroon01gezondheidsbeleving p
-                    where p.vragenlijstid =  ?");
-    $result->bind_param("i", $vragenlijst_id);
-    $result->execute();
-    $result = $result->get_result()->fetch_assoc();
+    $result = checkIfPatternExists($vragenlijstId);
 
     if ($result != null) {
         //update
