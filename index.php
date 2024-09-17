8bcd59a2a448d3f39ef58a078d11dfa8cb09a9f0
Matilda Smets <matilda.smets@student.gildeopleidingen.nl>
2024-09-17 11:09:10 +0200

    Kleine fix voor verkeerde redirect

--- index.php
+++ index.php
@@ -3,7 +3,7 @@ session_start();
 include_once 'database/DatabaseConnection.php';
 $medewerkerId = $_SESSION['loggedin_id'];
 if ($medewerkerId == null) {
-    header("Location: ../inloggen");
+    header("Location: inloggen.php");
 }
 
 $result = DatabaseConnection::getConn()->prepare("SELECT clientid FROM verzorgerregel WHERE medewerkerid = ?");
