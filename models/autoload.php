71f0a6ec9be642e0744190dcf1b7f7dfb260abef
Matilda Smets <matilda.smets@student.gildeopleidingen.nl>
2024-10-28 09:18:35 +0100

    Aanmaking models map voor issue #377

---
+++ models/autoload.php
@@ -0,0 +1,8 @@
+<?php
+spl_autoload_register(function ($class_naam) {
+    $file = __DIR__ . '/' . str_replace('\\', '/', $class_naam) . '.php';
+    if (file_exists($file)) {
+        require_once $file;
+    }
+});
+?>
