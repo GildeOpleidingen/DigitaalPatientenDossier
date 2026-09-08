e156397a5e8514c12b438c4f41ced40b31863c29
beerkietel-cmyk <beerkietel@gmail.com>
2026-09-08 13:45:37 +0200

    commit

---
+++ default-config.php
@@ -0,0 +1,10 @@
+<?php
+
+class config
+{
+    public static string $host = "";
+    public static string $username = "";
+    public static string $pass = "";
+    public static string $db = "";
+}
+
